0304d687e492d6dcd42a4b57b499129f29903937
osoka46 <osman.bozdag@yahoo.com>
2023-10-21 13:12:06 +0200

    light mode and slide add

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const arrows=document.querySelectorAll('.bi-chevron-right');
 const movieList=document.querySelectorAll('.movie-list');
+const screenWidt=document.documentElement.clientWidth;
 
 arrows.forEach((arrow,index)=>{
-    arrow.addEventListener('click',(event)=>{
-        console.log(movieList[index].computedStyleMap().get('transform'));
+    let clickCounter=0;
+    const movieNumber=movieList[index].querySelectorAll('.movie-item').length;
+
+    arrow.addEventListener('click',function(){
+        clickCounter++;
+
+        if(movieNumber-(screenWidt/300+clickCounter)>=0)
+        {
+            movieList[index].style.transform=`translateX(${movieList[index].computedStyleMap().get('transform')[0].x.value-300}px)`;
+        }else{
+            movieList[index].style.transform='translateX(0)';
+        }
+    });
+});
+
+const ball=document.querySelector('.toggle-ball');
+const items=document.querySelectorAll('.container,.profile-text,.sidebar,.sidebar i,.navbar,.menu-list li a,.movie-list-title,.toggle,.toggle-ball');
+
+ball.addEventListener('click',function(){
+    items.forEach(item=>{
+        item.classList.toggle('active');
     })
 })
